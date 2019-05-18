6ae3b9a632cdae6425fcfe6acc59f06dcb17b550
Luca Mariani <Luca Mariani@DESKTOP-AIKG756>
2019-05-18 20:48:25 +0200

    Aggiunta del colore alla sabbia

--- fileEsercizio/NB_ProcessingBall/src/nb_processingball/Sabbia.java
+++ fileEsercizio/NB_ProcessingBall/src/nb_processingball/Sabbia.java
@@ -1,42 +1,64 @@
+
+import java.awt.Color;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Luca Mariani
  */
 public class Sabbia {
-    
-    /** indica l'altezza della sabbia a sinistra nella scatola */
-    private float altezzaOvest; 
-    
-    /** indica l'altezza della sabbia a destra nella scatola */
-    private float altezzaEst; 
-    
-    /** indica l'altezza della sabbia sul lato verso nord della scatola */
-    private float altezzaNord; 
-    
-    /** indica l'altezza della sabbia sul lato verso sud della scatola */
-    private float altezzaSud; 
-    
-    /** indica lo spostamento lungo l'asse X della sabbia (OVEST o EST) */
-    private float uscitaX; 
-   
-    /** indica lo spostamento lungo l'asse Y della sabbia (NORD o SUD) */
-    private float uscitaY; 
-    
-    /** indica la velocità della sabbia quandi durante lo spostamento */
-    private float velocita; 
-    
+
     /**
-     * @brief Costruttore vuoto che inizializza gli attributi con valori predefiniti.
-     * 
+     * indica l'altezza della sabbia a sinistra nella scatola
+     */
+    private float altezzaOvest;
+
+    /**
+     * indica l'altezza della sabbia a destra nella scatola
+     */
+    private float altezzaEst;
+
+    /**
+     * indica l'altezza della sabbia sul lato verso nord della scatola
+     */
+    private float altezzaNord;
+
+    /**
+     * indica l'altezza della sabbia sul lato verso sud della scatola
+     */
+    private float altezzaSud;
+
+    /**
+     * indica lo spostamento lungo l'asse X della sabbia (OVEST o EST)
+     */
+    private float uscitaX;
+
+    /**
+     * indica lo spostamento lungo l'asse Y della sabbia (NORD o SUD)
+     */
+    private float uscitaY;
+
+    /**
+     * indica la velocità della sabbia quandi durante lo spostamento
+     */
+    private float velocita;
+
+    /**
+     * indica il colore della sabbia
+     */
+    private Color colore;
+
+    /**
+     * @brief Costruttore vuoto che inizializza gli attributi con valori
+     * predefiniti.
+     *
      */
     public Sabbia() {
-        altezzaOvest = 17f; 
+        altezzaOvest = 17f;
         altezzaEst = 17f;
         altezzaNord = 17f;
         altezzaSud = 17f;
@@ -45,15 +67,17 @@ public class Sabbia {
         uscitaY = 0f;
 
         velocita = 0f;
+
+        colore = new Color(224, 161, 117);
     }
-    
+
     /**
      * @brief Costruttore con parametri.
      * @param altezzaNord
      * @param altezzaSud
      * @param altezzaOvest
      * @param altezzaEst
-     * @param velocita 
+     * @param velocita
      */
     public Sabbia(float altezzaNord, float altezzaSud, float altezzaOvest, float altezzaEst, float velocita) {
         this.altezzaNord = altezzaNord;
@@ -61,70 +85,77 @@ public class Sabbia {
         this.altezzaOvest = altezzaOvest;
         this.altezzaEst = altezzaEst;
         this.velocita = velocita;
+
+        uscitaX = 0f;
+        uscitaY = 0f;
+
+        colore = new Color(224, 161, 117);
     }
-    
+
     /**
-     * 
-     * @return altezzaNord 
+     *
+     * @return altezzaNord
      */
     public float getAltezzaNord() {
         return altezzaNord;
     }
-    
+
     /**
-     * 
+     *
      * @return altezzaSud
      */
     public float getAltezzaSud() {
         return altezzaSud;
     }
-    
+
     /**
-     * 
+     *
      * @return altezzaOvest
      */
     public float getAltezzaOvest() {
         return altezzaOvest;
     }
-    
+
     /**
-     * 
+     *
      * @return altezzaEst
      */
     public float getAltezzaEst() {
         return altezzaEst;
     }
-    
+
     /**
-     * 
+     *
      * @return uscitaX
      */
     public float getUscitaX() {
         return uscitaX;
     }
-    
+
     /**
-     * 
-     * @return uscitaY 
+     *
+     * @return uscitaY
      */
     public float getUscitaY() {
         return uscitaY;
     }
-    
+
     /**
-     * 
+     *
      * @return velocita
      */
     public float getVelocita() {
         return velocita;
     }
-    
+
     /**
-     * @brief Metodo che aggiorna il valore dell'altezza della sabbia su tutti i lati della scatola (se tutte le altezza valgono 0 significa che la scatola è priva di sabbia).
+     * @brief Metodo che aggiorna il valore dell'altezza della sabbia su tutti i
+     * lati della scatola (se tutte le altezza valgono 0 significa che la
+     * scatola è priva di sabbia).
      * @param altezzaNord
      * @param altezzaSud
      * @param altezzaOvest
-     * @param altezzaEst 
+     * @param altezzaEst
      */
     public void aggiornaAltezze(float altezzaNord, float altezzaSud, float altezzaOvest, float altezzaEst) {
         this.altezzaNord = altezzaNord;
@@ -132,11 +163,13 @@ public class Sabbia {
         this.altezzaOvest = altezzaOvest;
         this.altezzaEst = altezzaEst;
     }
-    
+
     /**
-     * @brief Metodo che controlla se l'altezza della sabbia è maggiore o meno dell'altezza della scatola
+     * @brief Metodo che controlla se l'altezza della sabbia è maggiore o meno
+     * dell'altezza della scatola
      * @param altezzaScatola
-     * @return true se l'altezza della sabbia in qualche lato è maggiore dell'altezza della scatola, altrimenti false
+     * @return true se l'altezza della sabbia in qualche lato è maggiore
+     * dell'altezza della scatola, altrimenti false
      */
     private boolean uscitaSabbia(int altezzaScatola) {
         if (altezzaOvest > altezzaScatola || altezzaEst > altezzaScatola
@@ -147,14 +180,15 @@ public class Sabbia {
         }
 
     }
-    
+
     /**
-     * @brief Metodo che definisce le direzioni da cui la sabbia può uscire dalla scatola.
+     * @brief Metodo che definisce le direzioni da cui la sabbia può uscire
+     * dalla scatola.
      */
     public enum Direzione {
         NESSUNA, NORD, SUD, EST, OVEST;
     }
-    
+
     /**
      * @brief Metodo che controlla in che direzione esce la sabbia, se esce.
      * @param altezzaScatola
@@ -170,8 +204,7 @@ public class Sabbia {
                 } else {
                     dir = Direzione.EST;
                 }
-            }
-            else if (uscitaY != 0) {
+            } else if (uscitaY != 0) {
                 if (uscitaY >= 1) {
                     dir = Direzione.NORD;
                 } else {
@@ -185,7 +218,7 @@ public class Sabbia {
 
         return dir;
     }
-    
+
     /**
      * @brief Metodo che visualizza tutte le informazioni relative alla sabbia.
      */
